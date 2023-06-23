8d731634c29c270ed616f6f936680569475581c4
Mzwandile <mzwandilethatojunior@gmail.com>
2023-06-23 14:59:41 +0200

    project update

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Live with Confidence</h1>
+      <p>José Mourinho brings confidence to pan-African Sanlam campaign.</p>
+      <button>View project</button>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+const Navbar = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <ul>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Industries</a>
+        </li>
+        <li>
+          <a href="#">Cases</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+      <button>Let's Talk</button>
+    </div>
+  );
+};
+
+export default Navbar;
